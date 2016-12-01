84d9ebe6cc1ff6a77a914a48f41b15d021eed645
Chloe Phua <cqphua@gmail.com>
2016-12-01 18:00:08 +1100

    added talk descriptions

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,18 +22,28 @@ speaker_name = [
   'Chloe Phua',
   'Kerrie Batrouney'];
 speaker_desc = [
-  
+  "Ari speaks design in volumes, but in simple terms too.",
+  "Bruno pretty much spoke Ruby before English. He spoke it before he was born and he speaks it still.",
+  "Catherine volunteers as a open-source gem creator that fixes all our dev problems. Bless her soul.",
+  "Charith saves kittens by night and writes stories about Ruby by the day.",
+  "The numbers don't lie and neither does Dean, the data expert. Know your customers, know your numbers.",
+  "Duyen is a creative soul, constantly challenging the limits of CSS and coding today.",
+  "EJ is a professional web explorer. Also a sasquatch observer in her spare time. Please get in touch if you sight a sasquatch.",
+  "James "
+
+
+
 
 ]
 talk_title = [
   "What's New in HTML 5.1",
   "Developing a Wizard or Multi-Steps forms in Rails",
   "The End of Native Apps",
-  "Managing State in Aurelia: How to Use Aurelia with Redux",
+  "Taking Care of State in Aurelia with Redux",
   "How to Track User Behavior with Google Analytics Events",
-  "Introduction to Chatbots",
+  "Creating Chats with Chatbots",
   "Promoting Pair Programming and Paired Practice",
-  "Deepstream: an Open-Source Server for Building Realtime Apps",
+  "Deepstream: an Introduction",
   "JS Fatigue and Dealing with It",
   "Treading Thredded v0.9",
   "Nextcloud, the Latest Cloud",
@@ -43,15 +53,34 @@ talk_title = [
   "PHP 7.1 - 8 New Features you need to know",
   "Pure CSS - Stripping Away the Unnecessary",
   "Getting Rapt with Raptor",
-  "How I Explained Backbone.js to Java Developers in 45 Minutes",
   "How to Learn Angular",
-  "Free Yourself! The Art of Freelancing",
-  "Listening for Great UX",
   "Advanced Platforms Means New Responsive Designs",
   "Building a Simple Network Stack in Ruby",
   "Exploring Endlessly with ES6"
   ];
-talk_desc = [];
+talk_desc = [
+  "Get up to speed with the latest features of HTML 5.1 and what we can expect from HTML 5.2.",
+  "Maneuver your way through multi-step forms with Bruno before records are actually saved to the database.",
+  "Catherine explores the impending death of native apps and where HTML5 fails where native apps still succeeds.",
+  "Charith talks about Aurelia, the next-generation open-source JavaScript client framework and how it can be paired with Redux.",
+  "Google Analytics expert Dean talks about stalking your favourite users' behaviours.",
+  "Duyen carries us through the basic mechanics of creating a chatbot. If you're lonely, please attend.",
+  "EJ talks about the perks of pair programming and how it actually enlivens our souls and increases productivity",
+  "James gets deep with Deepstream,  an Open-Source Server for Building Realtime Apps",
+  "Jason talks about JS Fatigue, what we can learn and how to get well again.",
+  "Mr Chan encourages us to get online with Thredded, the best possible Rails 4.2+ forum in existence.",
+  "Nextcloud, is it the best cloud? Is it the next cloud? Or is it really the last cloud... Leon lets us know.",
+  "Melissa advises with her expert opinion on writing code that won't get you sick!",
+  "Ever wondered how to create a port scanner? Micah knows and Micah will tell. Thanks Micah.",
+  "Nick shows us the range of hamburger menus and new hamburger menus that can be hammed out.",
+  "Persist with Paul Morello and PHP. Please.",
+  "Remove unnecessary CSS with PurifyCSS and Grunt with Paul Williams.",
+  "Pedro pleases us with Phusion Passenger 5 which has an innovative and optimized HTTP engine. Get up to speed with Raptor.",
+  "Learn how to learn Angular with Sean Karita. From the basics to the very best tips.",
+  "How responsive should responsive design be? What are the limits? Tim talks us through incorporating it with the more advanced plaforms of today.",
+  "Chloe takes us back to basics with a simple network stack.",
+  "Kerrie takes us on an adventure with ES6. Explore. Learn. Grow. Kerrie knows."
+  ];
 talk_genre = [
   "HTML/CSS",
   "Ruby",
@@ -71,9 +100,6 @@ talk_genre = [
   "CSS",
   "Ruby",
   "JavaScript",
-  "JavaScript",
-  "General",
-  "UX",
   "HTML/CSS",
   "Ruby",
   "JavaScript"
