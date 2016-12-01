fbde7ec3b86c7ba42e90c37ebfa1534bd1b64bbd
Chloe Phua <cqphua@gmail.com>
2016-12-01 17:14:46 +1100

    commenced Session seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,90 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#create session info
+speaker_name = [
+  'Ari Crosby',
+  'Bruno Rampinelli',
+  'Catherine Gracey',
+  'Charith Perera',
+  'Dean Birznieks',
+  'Duyen Ho',
+  'Elizabeth Jones',
+  'James Mah',
+  'Jason Pompon',
+  'Ka Seng Chan',
+  'Leon Falvey',
+  'Melissa Quach',
+  'Micah Boyd',
+  'Nick Mallabo',
+  'Paul Morello',
+  'Paul Williams',
+  'Pedro Araujo',
+  'Sean Karita',
+  'Tim Walter',
+  'Chloe Phua',
+  'Kerrie Batrouney'];
+speaker_desc = [
+  
+
+]
+talk_title = [
+  "What's New in HTML 5.1",
+  "Developing a Wizard or Multi-Steps forms in Rails",
+  "The End of Native Apps",
+  "Managing State in Aurelia: How to Use Aurelia with Redux",
+  "How to Track User Behavior with Google Analytics Events",
+  "Introduction to Chatbots",
+  "Promoting Pair Programming and Paired Practice",
+  "Deepstream: an Open-Source Server for Building Realtime Apps",
+  "JS Fatigue and Dealing with It",
+  "Treading Thredded v0.9",
+  "Nextcloud, the Latest Cloud",
+  "HOW TO WRITE CLEAN, TESTABLE CODE",
+  "How to Write a Port Scanner in Ruby",
+  "New Menu Options for the Hamburger Menu",
+  "PHP 7.1 - 8 New Features you need to know",
+  "Pure CSS - Stripping Away the Unnecessary",
+  "Getting Rapt with Raptor",
+  "How I Explained Backbone.js to Java Developers in 45 Minutes",
+  "How to Learn Angular",
+  "Free Yourself! The Art of Freelancing",
+  "Listening for Great UX",
+  "Advanced Platforms Means New Responsive Designs",
+  "Building a Simple Network Stack in Ruby",
+  "Exploring Endlessly with ES6"
+  ];
+talk_desc = [];
+talk_genre = [
+  "HTML/CSS",
+  "Ruby",
+  "JavaScript",
+  "JavaScript",
+  "General",
+  "JavaScript",
+  "General",
+  "Servers",
+  "JavaScript",
+  "Ruby",
+  "General",
+  "General",
+  "Ruby",
+  "HTML/CSS",
+  "PHP",
+  "CSS",
+  "Ruby",
+  "JavaScript",
+  "JavaScript",
+  "General",
+  "UX",
+  "HTML/CSS",
+  "Ruby",
+  "JavaScript"
+  ];
+talk_time = [];
+speaker_img = [];
+venue = [];
+seats = [];
+social_media = [];
+
+
+
+
+#create 2 dummy attendees
