ba498d664ed13f38295d413fda378a8782d8f0c8
Chloe Phua <cqphua@gmail.com>
2016-12-05 17:40:50 +1100

    add images

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
     })
     .click(function(event) {
       $('.info').empty();
-      console.log($(event.target).data("id"))
+      console.log($(event.target).data("id"));
       var selectedID = $(event.target).data("id");
       $.ajax({
         url: "/api/displayspeakers",
@@ -62,6 +62,8 @@ $(document).ready(function() {
       })
       .done(function(response) {
         var info = response;
+        console.log("click on overlay:" + response);
+        console.log(response)
         var source = $("#overlay").html();
         var template = Handlebars.compile(source);
 
