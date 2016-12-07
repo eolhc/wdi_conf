32a83b1dfb14494ed304f44df28869b707810cbb
Pedro Araujo <ppteixeira@outlook.com>
2016-12-07 15:29:44 +1100

    add attendee and session booking

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -14,34 +14,39 @@ class ChargesController < ApplicationController
   def create
 
 
-    Stripe.api_key = ""
+    Stripe.api_key = "sk_test_iELhy6uC7yc8028vIjsLjN4t"
     # Get the credit card details submitted by the form
     token = params[:stripeToken]
 
     # Create a charge: this will charge the user's card
     begin
-      customer = Stripe::Customer.create(
-
-      )
-
       charge = Stripe::Charge.create(
-      :amount => 500, # Amount in cents
-      :currency => "aud",
-      :source => token,
-      :description => "WDI Conf 2k16 - Ticket"
+        :amount => 500, # Amount in cents
+        :currency => "aud",
+        :source => token,
+        :description => "WDI Conf 2k16 - Ticket"
       )
     rescue Stripe::CardError => e
       # The card has been declined
     end
-
     if charge.status == 'succeeded'
-      redirect_to({
-        controller: 'attendee',
-        action: 'create' },
-        'params[:first_name]' => params[:first_name],
-        'params[:second_name]' => params[:second_name],
-        'params[:email]' => params[:email],
-        'params[:phone]' => params[:phone] )
+      # redirect_to({
+      #   controller: 'attendee',
+      #   action: 'create_attendee' ,
+      #   'params[:first_name]' => params[:first_name],
+      #   'params[:second_name]' => params[:second_name],
+      #   'params[:email]' => params[:email],
+      #   'params[:phone]' => params[:phone] })
+
+      redirect_to :controller => 'attendee',
+                  :action => 'create_attendee',
+                  :method => 'post',
+                  :first_name => params[:first_name],
+                  :last_name => params[:last_name],
+                  :email => params[:email],
+                  :phone => params[:phone],
+                  :sessions => params[:sessions]
+
 
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#index"
-  resources :attendee
+  # resources :attendee
 
   get "/api/displayspeakers", to: "api/session#getSession"
 
   post "/api/attendee/new", to: "api/attendee#new"
   post "/api/submitbooking", to: "api/booking#new"
-  post "/payment", to: "pages#payment"
+  # post "/payment", to: "pages#payment"
   post "/charges", to: "charges#create"
+  get "/attendee", to: "attendee#create_attendee"
+
 
 
 end
