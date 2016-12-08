42e186da80369d699a7b9ba374ecb96e11c77b5e
Chloe Phua <cqphua@gmail.com>
2016-12-08 16:22:58 +1100

    fixed missing itinerary error

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -83,135 +83,140 @@ var displaySpeakers = function(response) {
 
 }
 
-$(document).on("keydown",function(e) {
-if (e.which == 27) {
-  console.log('escape was hit!')
-  $('.overlay').animate({width:'toggle'},1000);
+$(document).ready(function() {
+
+
+  $(document).on("keydown",function(e) {
+  if (e.which == 27) {
+    console.log('escape was hit!')
+    $('.overlay').animate({width:'toggle'},1000);
+    }
+  })
+
+
+
+  //display slots for the itinerary
+  var times = [9, 10, 11, 12, 13, 14, 15, 16]
+  var innertext = ["9:00","10:00","11:00","L U N C H","13:00","14:00","15:00","16:00"]
+  var source = $("#timeslot").html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < times.length; i++) {
+    timeslot = {
+      talktime: times[i],
+      text: innertext[i]
+    }
+    $(".itinerary").append(template(timeslot))
   }
-})
 
 
+  // Make 'timeslot' Draggable and set properties
+  $(".timeslot").droppable({
+    hoverClass: hoverTimeslot,
+    out: function(){
+      $(this).css("background-color","")
+    },
+    tolerance: "pointer",
 
-//display slots for the itinerary
-var times = [9, 10, 11, 12, 13, 14, 15, 16]
-var innertext = ["9:00","10:00","11:00","L U N C H","13:00","14:00","15:00","16:00"]
-var source = $("#timeslot").html();
-var template = Handlebars.compile(source);
-for (var i = 0; i < times.length; i++) {
-  timeslot = {
-    talktime: times[i],
-    text: innertext[i]
+    drop: placeSession
+    // scope: function() {
+    //   this.attr('time')
+    // }
+  })
+
+  function hoverTimeslot( event, ui ) {
+    var slotTime = $(this).attr('time')
+    var cardTime = $('.ui-draggable-helper').attr('time')
+    if ( slotTime == cardTime ) {
+      return 'correct'
+    }else {
+      return 'wrong'
+    }
   }
-  $(".itinerary").append(template(timeslot))
-}
 
 
-// Make 'timeslot' Draggable and set properties
-$(".timeslot").droppable({
-  hoverClass: hoverTimeslot,
-  out: function(){
-    $(this).css("background-color","")
-  },
-  tolerance: "pointer",
+  function placeSession( event, ui ) {
+    var slotTime = $(this).attr('time')
+    var cardTime = ui.draggable.attr('time')
 
-  drop: placeSession
-  // scope: function() {
-  //   this.attr('time')
-  // }
-})
+    // If the card was dropped to the correct slot,
+    // change the card colour, position it directly
+    // on top of the slot, and prevent it being dragged
+    // again
 
-function hoverTimeslot( event, ui ) {
-  var slotTime = $(this).attr('time')
-  var cardTime = $('.ui-draggable-helper').attr('time')
-  if ( slotTime == cardTime ) {
-    return 'correct'
-  }else {
-    return 'wrong'
+    if ( slotTime == cardTime ) {
+      //ui.draggable.addClass( 'correct' );
+      //ui.draggable.draggable( 'disable' );
+      //$(this).droppable( 'disable' );
+      //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
+      //ui.draggable.draggable( 'option', 'revert', false );
+
+
+      //take text within the session box that was selected for dragging
+      //ui.draggable.html()
+      //put text within the timeslot that it was dragged into
+      $(this).html(
+        '<i id="remove-speaker" class="fa fa-minus-square remove-speaker" aria-hidden="true"></i>'
+        +ui.draggable.html()
+        + '<p>'
+        +'Location: '
+        + ui.draggable.attr('venue').split('-').join(' ')
+        + '</p>'
+      )
+      .css({
+        "text-align": "left",
+      });
+
+      $(this).find("p").css({
+        "margin-left": "7em",
+        "margin-top": "0em",
+        "margin-bottom": "0em"
+      })
+
+      //button to remove speaker from the timeslot
+      $('.remove-speaker').on("click",function(e) {
+        console.log('removed');
+        this.parentElement.style.textAlign = 'center';
+        $(this.parentElement).html('<p>'+ this.parentElement.attributes.time.value +':00</p>')
+
+      })
+
+      // return false;
+    }else {
+      // return true;
+    }
   }
-}
 
 
-function placeSession( event, ui ) {
-  var slotTime = $(this).attr('time')
-  var cardTime = ui.draggable.attr('time')
 
-  // If the card was dropped to the correct slot,
-  // change the card colour, position it directly
-  // on top of the slot, and prevent it being dragged
-  // again
+  //scrolling menu
+  $("#myTopnav li").click(function(){
+    scrollTo($(this).data('location'));
+  });
+  //scrollTo($("#sponsors"));
 
-  if ( slotTime == cardTime ) {
-    //ui.draggable.addClass( 'correct' );
-    //ui.draggable.draggable( 'disable' );
-    //$(this).droppable( 'disable' );
-    //ui.draggable.position( { of: $(this), my: 'left top', at: 'left top' } );
-    //ui.draggable.draggable( 'option', 'revert', false );
-
-
-    //take text within the session box that was selected for dragging
-    //ui.draggable.html()
-    //put text within the timeslot that it was dragged into
-    $(this).html(
-      '<i id="remove-speaker" class="fa fa-minus-square remove-speaker" aria-hidden="true"></i>'
-      +ui.draggable.html()
-      + '<p>'
-      +'Location: '
-      + ui.draggable.attr('venue').split('-').join(' ')
-      + '</p>'
-    )
-    .css({
-      "text-align": "left",
-    });
-
-    $(this).find("p").css({
-      "margin-left": "7em",
-      "margin-top": "0em",
-      "margin-bottom": "0em"
-    })
-
-    //button to remove speaker from the timeslot
-    $('.remove-speaker').on("click",function(e) {
-      console.log('removed');
-      this.parentElement.style.textAlign = 'center';
-      $(this.parentElement).html('<p>'+ this.parentElement.attributes.time.value +':00</p>')
-
-    })
-
-    // return false;
-  }else {
-    // return true;
+  function scrollTo(element){
+    $('html, body').animate({
+      scrollTop: $(element).offset().top
+    },  1000);
   }
-}
+  // Fix nav bar menu on top when scroll
+  var twentyEms = Number(getComputedStyle(document.body, "").fontSize.match(/(\d*(\.\d*)?)px/)[1]) * 20;
+  $(window).scroll(function () {
+   if ($(window).scrollTop() > twentyEms) {
+     $('nav').addClass('nav-fixed');
+   }
+   if ($(window).scrollTop() < twentyEms) {
+     $('nav').removeClass('nav-fixed');
+   }
+  });
 
 
+  // button to close speaker info
+  $('#close-window').on("click",function(e) {
+    $('.overlay').animate({width:'toggle'},1000)
+  })
 
-//scrolling menu
-$("#myTopnav li").click(function(){
-  scrollTo($(this).data('location'));
+  //tooltipped to drag session box
+  $('.tooltipped').tooltip({delay: 50});
+
 });
-//scrollTo($("#sponsors"));
-
-function scrollTo(element){
-  $('html, body').animate({
-    scrollTop: $(element).offset().top
-  },  1000);
-}
-// Fix nav bar menu on top when scroll
-var twentyEms = Number(getComputedStyle(document.body, "").fontSize.match(/(\d*(\.\d*)?)px/)[1]) * 20;
-$(window).scroll(function () {
- if ($(window).scrollTop() > twentyEms) {
-   $('nav').addClass('nav-fixed');
- }
- if ($(window).scrollTop() < twentyEms) {
-   $('nav').removeClass('nav-fixed');
- }
-});
-
-
-// button to close speaker info
-$('#close-window').on("click",function(e) {
-  $('.overlay').animate({width:'toggle'},1000)
-})
-
-//tooltipped to drag session box
-$('.tooltipped').tooltip({delay: 50});
