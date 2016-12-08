85f15531b9cc7e5229ad7d9414939907f0f8428f
Chloe Phua <cqphua@gmail.com>
2016-12-08 18:15:45 +1100

    add itinerary display upon successful payment

--- app/controllers/api/booking_controller.rb
+++ app/controllers/api/booking_controller.rb
@@ -2,24 +2,30 @@ class Api::BookingController < ApplicationController
 
   def displayItinerary
 
-    # selected_sessions = params["sessions"].split(',')
+    selected_sessions = params["sessionID"]
     #all _sessions = response
-    # response = [];
-    response = Session.all
+    all_sessions = [];
+    # response = selected_sessions
 
-    # selected_sessions.each do |session,index|
-    #   session_details = Session.find(session.to_i)
-    #   response[index] = {
-    #     session_details.speaker_name = params[:speaker_name]
-    #     session_details.venue = params[:venue]
-    #     session_details.talk_title = params[:talk_title]
-    #     session_details.talk_time = params[:talk_time]
-    #   }
-    # end
+    selected_sessions.each do |session|
+      timeslot = {}
 
-    render json: response
+      session_details = Session.find(session.to_i)
+
+      timeslot["speaker_name"] = session_details.speaker_name
+      timeslot["talk_title"] = session_details.talk_title
+      timeslot["talk_time"] = session_details.talk_time
+      # timeslot["venue"] = session_details.venue
+
+      all_sessions << timeslot
+    end
+
+    render json: all_sessions
   end
-
-
+#
+# session1 = {
+#   speaker_name: "name",
+#   talk_title: "talk_title"
+# }
 
 end
