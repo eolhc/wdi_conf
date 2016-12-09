a152d25a38cb8cdcb4e8b96251a091765a547235
Dean Birznieks <deanb100@gmail.com>
2016-12-09 12:06:36 +1100

    add email functionality in progress

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,28 +3,27 @@ class PagesController < ApplicationController
   def success
     @sessions = params["sessions"]
     @attendee_id = params["attendee_id"]
-    @attendee = Attendee.find(@attendee_id)
+    attendee = Attendee.find(@attendee_id)
+
+    create(attendee)
     render :success
   end
 
-end
-
-
 # To trigger the event that will send an email to an attendee.
-def create
-  @attendee = Attendee.new(user_params)
+  def create (attendee)
 
-  respond_to do |format|
-    if @attendee.save
+    # respond_to do |format|
 
       # Sends email to user when user is created.
-      ItineraryMailer.sample_email(@attendee).deliver
+      ItineraryMailer.sample_email(attendee).deliver_now
+      raise
 
-      format.html { redirect_to @attendee, notice: 'Attendee was successfully created.' }
-      format.json { render :show, status: :created, location: @attendee }
-    else
-      format.html { render :new }
-      format.json { render json: @attendee.errors, status: :unprocessable_entity }
-    end
+      #
+      # format.html { redirect_to attendee, notice: 'Attendee was successfully created.' }
+      # format.json { render :show, status: :created, location: attendee }
+    # else
+    #   format.html { render :new }
+    #   format.json { render json: @attendee.errors, status: :unprocessable_entity }
+    # end
   end
 end
